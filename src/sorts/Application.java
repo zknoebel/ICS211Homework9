a6f8f92216ac506a393b8bb60aa23e60405dfb8c
Zac <zknoebel@gmail.com>
2016-11-30 19:55:27 -1000

    modified:   src/sorts/Application.java 	modified:   src/sorts/ArraySort.java

--- src/sorts/Application.java
+++ src/sorts/Application.java
@@ -1,52 +1,110 @@
 package sorts;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 
 public class Application {
-	public static void main(String[] args)throws java.io.IOException{
-		BufferedReader br0 = new BufferedReader(new FileReader("src//txtFiles//A10-1000-words.txt"));
-		BufferedReader br1 = new BufferedReader(new FileReader("src//txtFiles//A10-5000-words.txt"));
-		BufferedReader br2 = new BufferedReader(new FileReader("src//txtFiles//A10-10000-words.txt"));
-
-		ArraySort sorter = new ArraySort();
-		CompareStrings comp = new CompareStrings();
-		
-		String[] file0 = new String[1000];
-		String[] file1 = new String[5000];
-		String[] file2 = new String[10000];
-		
-		for(int i = 0; i < 1000; i ++){
-			file0[i] = br0.readLine();
-		}
-		
-			for(int i = 0; i < 5000; i ++){
-			file1[i] = br1.readLine();
-		}
-
-			for(int i = 0; i < 10000; i ++){
-			file2[i] = br2.readLine();
-		}
-			
-		
-			sorter.heapSort(file0, comp);
-			for(int i = 0; i < 1000; i ++){
-				System.out.println(file0[i]);
-			}
-			System.out.println("");
-			System.out.println("Time: " + sorter.getSortTime() + " nano seconds");
-			System.out.println("Comparisons: " + sorter.getCompareCount());
-			System.out.println("Swaps: " + sorter.getSwapCount());
-			System.out.println("");
+  public static void main(String[] args) throws java.io.IOException {
+    String[] file0 = new String[1000];
+    String[] file1 = new String[5000];
+    String[] file2 = new String[10000];
 
 
-			sorter.heapSort(file1, comp);
+    heapSortPrint(file0, file1, file2);
+    mergeSortPrint(file0, file1, file2);
+    quickSortPrint(file0, file1, file2);
 
-			sorter.heapSort(file2, comp);
+  }
 
-		br0.close();
-		br1.close();
-		br2.close();
-	}
+
+  public static void heapSortPrint(String[] file0, String[] file1, String[] file2) throws java.io.IOException {
+    ArraySort<String> sorter = new ArraySort<>();
+    CompareStrings comp = new CompareStrings();
+
+    setArrays(file0, file1, file2);
+
+    sorter.heapSort(file0, comp);
+    printResults("Heap Sort", sorter, file0);
+
+    sorter.heapSort(file1, comp);
+    printResults("Heap Sort", sorter, file1);
+
+    sorter.heapSort(file2, comp);
+    printResults("Heap Sort", sorter, file2);
+
+  }
+
+  public static void quickSortPrint(String[] file0, String[] file1, String[] file2) throws java.io.IOException {
+    ArraySort<String> sorter = new ArraySort<>();
+    CompareStrings comp = new CompareStrings();
+    
+     setArrays(file0, file1, file2);
+   
+    sorter.quickSort(file0, comp);
+    printResults("Quick Sort", sorter, file0);
+
+    sorter.quickSort(file1, comp);
+    printResults("Quick Sort", sorter, file1);
+
+    sorter.quickSort(file2, comp);
+    printResults("Quick Sort", sorter, file2);
+
+
+  }
+
+  public static void mergeSortPrint(String[] file0, String[] file1, String[] file2) throws java.io.IOException {
+    ArraySort<String> sorter = new ArraySort<>();
+    CompareStrings comp = new CompareStrings();
+    
+        setArrays(file0, file1, file2);
+
+    sorter.mergeSort(file0, comp);
+    printResults("Merge Sort", sorter, file0);
+
+    sorter.mergeSort(file1, comp);
+    printResults("Merge Sort", sorter, file1);
+
+    sorter.mergeSort(file2, comp);
+    printResults("Merge Sort", sorter, file2);
+
+
+  }
+
+  public static <E> void printResults(String sortName, ArraySort<E> sorter, E[] array){
+    for (int i = 0; i < array.length; i++) {
+      System.out.println(array[i]);
+    }
+    System.out.println("");
+    System.out.println(sortName);
+    System.out.println("Time: " + sorter.getSortTime() + " nano seconds");
+    System.out.println("Comparisons: " + sorter.getCompareCount());
+    System.out.println("Swaps: " + sorter.getSwapCount());
+    System.out.println("");
+
+
+  }
+
+  public static <E> void setArrays(E[] array0, E[] array1, E[] array2)throws java.io.IOException{
+    BufferedReader br0 = new BufferedReader(new FileReader("src//txtFiles//A10-1000-words.txt"));
+    BufferedReader br1 = new BufferedReader(new FileReader("src//txtFiles//A10-5000-words.txt"));
+    BufferedReader br2 = new BufferedReader(new FileReader("src//txtFiles//A10-10000-words.txt"));
+
+    for (int i = 0; i < 1000; i++) {
+      array0[i] = (E)br0.readLine();
+    }
+
+    for (int i = 0; i < 5000; i++) {
+      array1[i] = (E)br1.readLine();
+    }
+
+    for (int i = 0; i < 10000; i++) {
+      array2[i] = (E)br2.readLine();
+    }
+
+    br0.close();
+    br1.close();
+    br2.close();
+
+
+  }
 }
--- src/sorts/ArraySort.java
+++ src/sorts/ArraySort.java
@@ -5,219 +5,293 @@ import java.util.Comparator;
 /**
  * ArraySort Copyright (C) Zachery Knoebel 2016
  * 
- * ArraySort: This class provides the user with the ability to sort arrays using
- * bubble sort, insertion sort and selection sort. It also gives the user the
- * ability to print out the sorted array, number of comparisons made, number of
- * swaps made and the time it took to run the sort method.
+ * ArraySort: This class provides the user with the ability to sort arrays using bubble sort, insertion sort and
+ * selection sort. It also gives the user the ability to print out the sorted array, number of comparisons made, number
+ * of swaps made and the time it took to run the sort method.
  */
 public class ArraySort<E> {
 
-	private boolean finished = false;
-	private E e;
-	private int placeHolder = 0;
-	private long compareCount, swapCount;
-	private long sortTime, startTime;
+  private boolean finished = false;
+  private E e;
+  private int placeHolder = 0;
+  private long compareCount, swapCount;
+  private long sortTime, startTime;
 
-	public ArraySort() {
-	}
 
-	/*
-	 * sorts an array of numbers by "sinking" the large numbers to the "bottom"
-	 * or end of the list and "bubbling" the small numbers to the "top" or front
-	 * of the list
-	 * 
-	 * @param data: the unsorted array
-	 * 
-	 * @param compare: a comparator made for the specific type to be sorted
-	 */
-	public void bubbleSort(E[] data, Comparator<? super E> compare) {
+  public ArraySort() {
+  }
 
-		sortTime = 0;
-		startTime = System.nanoTime();
-		compareCount = 0;
-		swapCount = 0;
 
-		for (int i = 0; i < data.length - 1; i++) {
+  /*
+   * sorts an array of numbers by "sinking" the large numbers to the "bottom" or end of the list and "bubbling" the
+   * small numbers to the "top" or front of the list
+   * 
+   * @param data: the unsorted array
+   * 
+   * @param compare: a comparator made for the specific type to be sorted
+   */
+  public void bubbleSort(E[] data, Comparator<? super E> compare) {
 
-			finished = true;
+    sortTime = 0;
+    startTime = System.nanoTime();
+    compareCount = 0;
+    swapCount = 0;
 
-			for (int j = 0; j < data.length - 1 - i; j++) {
+    for (int i = 0; i < data.length - 1; i++) {
 
-				compareCount++;
+      finished = true;
 
-				if (compare.compare(data[j], data[j + 1]) > 0) {
+      for (int j = 0; j < data.length - 1 - i; j++) {
 
-					finished = false;
-					swapCount++;
-					e = data[j];
-					data[j] = data[j + 1];
-					data[j + 1] = e;
-				}
-			}
+        compareCount++;
 
-			if (finished == true) {
-				break;
-			}
-		}
+        if (compare.compare(data[j], data[j + 1]) > 0) {
 
-		sortTime = System.nanoTime() - startTime;
-		// printResults(data, "Bubble Sort");
+          finished = false;
+          swapCount++;
+          e = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = e;
+        }
+      }
 
-	}
+      if (finished == true) {
+        break;
+      }
+    }
 
-	/*
-	 * returns the number of comparisons that were made by the sort method that
-	 * ran last
-	 */
-	public long getCompareCount() {
-		return compareCount;
-	}
+    sortTime = System.nanoTime() - startTime;
+    // printResults(data, "Bubble Sort");
 
-	/*
-	 * returns the amount time it took for the most recently run sort method to
-	 * run.
-	 */
-	public long getSortTime() {
-		return sortTime;
-	}
+  }
 
-	/*
-	 * returns the amount of swaps that were made by the sort method that ran
-	 * last
-	 */
-	public long getSwapCount() {
-		return swapCount;
-	}
 
-	/*
-	 * sorts an array by looking at a number in an unsorted array and then
-	 * moving it to it's numerically sorted spot then it moves to the next
-	 * number in the array and does the same over and over again until it gets
-	 * to the end of the list
-	 * 
-	 * @param data: the unsorted array
-	 * 
-	 * @param compare: a comparator made for the specific type to be sorted
-	 */
-	public void insertionSort(E[] data, Comparator<? super E> compare) {
+  /*
+   * returns the number of comparisons that were made by the sort method that ran last
+   */
+  public long getCompareCount() {
+    return compareCount;
+  }
 
-		sortTime = 0;
-		startTime = System.nanoTime();
-		compareCount = 0;
-		swapCount = 0;
 
-		for (int i = 0; i < data.length - 1; i++) {
+  /*
+   * returns the amount time it took for the most recently run sort method to run.
+   */
+  public long getSortTime() {
+    return sortTime;
+  }
 
-			placeHolder = i;
 
-			while (compare.compare(data[placeHolder], data[placeHolder + 1]) > 0) {
+  /*
+   * returns the amount of swaps that were made by the sort method that ran last
+   */
+  public long getSwapCount() {
+    return swapCount;
+  }
 
-				compareCount++;
-				swapCount++;
 
-				e = data[placeHolder];
-				data[placeHolder] = data[placeHolder + 1];
-				data[placeHolder + 1] = e;
+  /*
+   * sorts an array by looking at a number in an unsorted array and then moving it to it's numerically sorted spot then
+   * it moves to the next number in the array and does the same over and over again until it gets to the end of the list
+   * 
+   * @param data: the unsorted array
+   * 
+   * @param compare: a comparator made for the specific type to be sorted
+   */
+  public void insertionSort(E[] data, Comparator<? super E> compare) {
 
-				if (placeHolder > 0) {
-					placeHolder--;
-				}
-			}
-			compareCount++;
-		}
+    sortTime = 0;
+    startTime = System.nanoTime();
+    compareCount = 0;
+    swapCount = 0;
 
-		sortTime = System.nanoTime() - startTime;
-		// printResults(data, "Insertion Sort");
-	}
+    for (int i = 0; i < data.length - 1; i++) {
 
-	/*
-	 * prints out the name of the sort used, the sorted array, time and number
-	 * of comparisons and swaps made for the used sort method
-	 */
-	public void printResults(E[] arrayToBePrinted, String title) {
+      placeHolder = i;
 
-		System.out.println(title);
-		System.out.print("The sorted array is ");
+      while (compare.compare(data[placeHolder], data[placeHolder + 1]) > 0) {
 
-		for (int i = 0; i < arrayToBePrinted.length; i++) {
+        compareCount++;
+        swapCount++;
 
-			System.out.print(arrayToBePrinted[i] + "  ");
-		}
+        e = data[placeHolder];
+        data[placeHolder] = data[placeHolder + 1];
+        data[placeHolder + 1] = e;
 
-		System.out.println("");
-		System.out.println("This method made " + compareCount + " comparisons.");
-		System.out.println("This method made " + swapCount + " swaps.");
-		System.out.println("This method took " + sortTime + " nano seconds to complete.");
-		System.out.println("");
-	}
+        if (placeHolder > 0) {
+          placeHolder--;
+        }
+      }
+      compareCount++;
+    }
 
-	/*
-	 * sorts an array of numbers by finding the smallest number in the array and
-	 * moving it to the front then it finds the next smallest number in the
-	 * array and moves it to the next spot this continues until the list is
-	 * sorted
-	 * 
-	 * @param data: the unsorted array
-	 * 
-	 * @param compare: a comparator made for the specific type to be sorted
-	 */
-	public void selectionSort(E[] data, Comparator<? super E> compare) {
+    sortTime = System.nanoTime() - startTime;
+    // printResults(data, "Insertion Sort");
+  }
 
-		startTime = System.nanoTime();
-		compareCount = 0;
-		swapCount = 0;
-		placeHolder = 0;
 
-		for (int i = 0; i < data.length - 1; i++) {
+  /*
+   * prints out the name of the sort used, the sorted array, time and number of comparisons and swaps made for the used
+   * sort method
+   */
+  public void printResults(E[] arrayToBePrinted, String title) {
 
-			e = data[i];
-			placeHolder = i;
+    System.out.println(title);
+    System.out.print("The sorted array is ");
 
-			for (int j = i; j < data.length; j++) {
+    for (int i = 0; i < arrayToBePrinted.length; i++) {
 
-				compareCount++;
+      System.out.print(arrayToBePrinted[i] + "  ");
+    }
 
-				if (compare.compare(e, data[j]) > 0) {
+    System.out.println("");
+    System.out.println("This method made " + compareCount + " comparisons.");
+    System.out.println("This method made " + swapCount + " swaps.");
+    System.out.println("This method took " + sortTime + " nano seconds to complete.");
+    System.out.println("");
+  }
 
-					e = data[j];
-					swapCount++;
-					placeHolder = j;
-				}
-			}
 
-			data[placeHolder] = data[i];
-			data[i] = e;
-		}
+  /*
+   * sorts an array of numbers by finding the smallest number in the array and moving it to the front then it finds the
+   * next smallest number in the array and moves it to the next spot this continues until the list is sorted
+   * 
+   * @param data: the unsorted array
+   * 
+   * @param compare: a comparator made for the specific type to be sorted
+   */
+  public void selectionSort(E[] data, Comparator<? super E> compare) {
 
-		sortTime = System.nanoTime() - startTime;
-	}
+    startTime = System.nanoTime();
+    compareCount = 0;
+    swapCount = 0;
+    placeHolder = 0;
 
-	public void heapSort(E[] data, Comparator<? super E> compare) {
-		startTime = System.nanoTime();
+    for (int i = 0; i < data.length - 1; i++) {
 
-		PriorityQueue<E> heapSorter = new PriorityQueue<>(compare);
-		heapSorter.setComp(0);
-		heapSorter.setSwap(0);
+      e = data[i];
+      placeHolder = i;
 
-		for (int i = 0; i < data.length; i++) {
+      for (int j = i; j < data.length; j++) {
 
-			heapSorter.add(data[i]);
-		}
+        compareCount++;
 
-		for (int i = 0; i < data.length; i++) {
-			data[i] = heapSorter.remove();
-		}
+        if (compare.compare(e, data[j]) > 0) {
 
-		compareCount = heapSorter.getComp();
-		swapCount = heapSorter.getSwap();
-		sortTime = System.nanoTime() - startTime;
-	}
+          e = data[j];
+          swapCount++;
+          placeHolder = j;
+        }
+      }
 
-	public void mergeSort(E[] data, Comparator<? super E> compare) {
+      data[placeHolder] = data[i];
+      data[i] = e;
+    }
 
-	}
+    sortTime = System.nanoTime() - startTime;
+  }
 
-	public void quickSort(E[] data, Comparator<? super E> compare) {
 
-	}
+  public void heapSort(E[] data, Comparator<? super E> compare) {
+    startTime = System.nanoTime();
+
+    PriorityQueue<E> heapSorter = new PriorityQueue<>(compare);
+    heapSorter.setComp(0);
+    heapSorter.setSwap(0);
+
+    for (int i = 0; i < data.length; i++) {
+
+      heapSorter.add(data[i]);
+    }
+
+    for (int i = 0; i < data.length; i++) {
+      data[i] = heapSorter.remove();
+    }
+
+    compareCount = heapSorter.getComp();
+    swapCount = heapSorter.getSwap();
+    sortTime = System.nanoTime() - startTime;
+  }
+
+
+  public void mergeSort(E[] data, Comparator<? super E> compare) {
+    startTime = System.nanoTime();
+    compareCount = 0;
+    swapCount = 0;
+
+    data = mergeSort(compare, splitLeft(data), splitRight(data));
+  }
+
+
+  private E[] mergeSort(Comparator<? super E> c, E[] left, E[] right) {
+    E[] output = (E[]) new Object[left.length + right.length];
+
+    if (left.length > 1) {
+      left = mergeSort(c, splitLeft(left), splitRight(left));
+    }
+
+    if (right.length > 1) {
+      right = mergeSort(c, splitLeft(right), splitRight(right));
+    }
+
+    int l = 0;
+    int r = 0;
+    int put = 0;
+
+    while (l < left.length && r < right.length) {
+      if (c.compare(left[l], right[r]) < 0) {
+        output[put++] = left[l++];
+      }
+      else {
+        output[put++] = right[r++];
+      }
+    }
+    while(l < left.length){
+      output[put++] = left[l++];
+    }
+    while(r < right.length){
+      output[put++] = right[r++];
+    }
+
+    return output;
+  }
+
+/**
+ * takes in an array and returns the first half of it as a separate array
+ * @param array: the array to be split
+ * @return: the first half of the array
+ */
+  private E[] splitLeft(E[] array) {
+    E[] output = (E[]) new Object[(int) (array.length / 2)];
+
+    for (int i = 0; i < (int) (array.length / 2); i++) {
+      output[i] = array[i];
+    }
+    return output;
+  }
+
+/**
+ * takes in an array and returns the second half of it as a separate array
+ * @param array: the array to be split
+ * @return: the second half of the array
+ */
+  private E[] splitRight(E[] array) {
+    E[] output = (E[]) new Object[array.length - (int) (array.length / 2)];
+
+    for (int i = (int) (array.length / 2); i < array.length; i++) {
+      output[i] = array[i - (int) (array.length / 2)];
+    }
+    return output;
+
+  }
+
+/**
+ * uses quick sort to sort an array of data
+ * 
+ * @param data: the array to be sorted
+ * @param compare: this will be used to compare the values in the array
+ */
+  public void quickSort(E[] data, Comparator<? super E> compare) {
+
+  }
 }
